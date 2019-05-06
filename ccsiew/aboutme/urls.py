198c5a5795bbfea64cadda50c564e04ba9883842
siewchongche <48756752+siewchongche@users.noreply.github.com>
2019-05-06 13:12:02 +0800

    Delete urls.py

--- ccsiew/aboutme/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'aboutme'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('django/', views.django, name='django'),
-]
