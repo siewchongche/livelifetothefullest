ba34350fb42988243ffbf41a64503a34f0f97eda
siewchongche <48756752+siewchongche@users.noreply.github.com>
2019-05-06 13:11:54 +0800

    Delete views.py

--- ccsiew/aboutme/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'aboutme/index.html')
-
-def django(request):
-    return render(request, 'aboutme/django.html')
